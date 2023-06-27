c69458bda04dcec269c21f51c11b003afe5176ea
Jonathan Molero <110490630+molerojj@users.noreply.github.com>
2023-06-27 15:08:24 -0400

    fix responsive home

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 import style from '../style/Styles.module.css';
 
 const Invite = () => {
-    return (
-        <div className={style.invite}>
-            <h2>Mr. & Mrs. Salisbury have the honor of inviting you to their wedding on <span className={style.resalt}>10/14/2023</span> At <span className={style.resalt}>3:00pm</span> At The Lincoln Cathedral, Rockport, Maine.</h2>
-            <p className={style.parrafo}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
-            <p className={style.parrafo}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div className={style.links}>
-                <Link className={style.linkStyle}>MORE DETAILS</Link>
-                <p>|</p>
-                <Link className={style.linkStyle}>RSVP NOW</Link>
-            </div>
-        </div>
-    )
+	return (
+		<div className={style.invite}>
+			<h2>Mr. & Mrs. Salisbury have the honor of inviting you to their wedding on <span className={style.resalt}>10/14/2023</span> At <span className={style.resalt}>3:00pm</span> At The Lincoln Cathedral, Rockport, Maine.</h2>
+			<p className={style.parrafo}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+			<p className={style.parrafo}>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<div className={style.links}>
+					<Link className={style.linkStyle}>MORE DETAILS</Link>
+					<p>|</p>
+					<Link className={style.linkStyle}>RSVP NOW</Link>
+			</div>
+		</div>
+	)
 }
 
-export default Invite
+export default Invite;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,58 @@ import Logo from '../assets/img/Logo.png';
 
 const Navbar = () => {
     return (
-        <div id='invite' className={style.Navbar}>
-            <ul>
-                <a href='#home' className={style.menu}>HOME</a>
-                <a href='#invite' className={style.menu}>INVITE</a>
-                <a href='#timing' className={style.menu}>TIMING</a>
-                <a href='#venue' className={style.menu}>VENUE</a>
-                <img className={style.logo} src={Logo} alt="logo-j&y" />
-                <a href='#story' className={style.menu}>STORY</a>
-                <a href='#gallery' className={style.menu}>GALLERY</a>
-                <a href='#details' className={style.menu}>DETAILS</a>
-                <a href='#rsvp' className={style.menu}>RSVP</a>
-            </ul>
-        </div>
+        <nav id='invite' className={`navbar navbar-expand-lg ${style.Navbar}`}>
+            <div className="container-fluid">
+                <button className={`navbar-toggler bg-white ${style.buttonToggle}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className={`navbar-nav ${style.navUl}`}>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#home">HOME</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#invite">INVITE</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#timing">TIMING</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#venue">VENUE</a>
+                        </li>
+                        <img className={style.logo} src={Logo} alt="logo-j&y" />
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#story">STORY</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#gallery">GALLERY</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#details">DETAILS</a>
+                        </li>
+                        <li className={`nav-item ${style.menu}`}>
+                            <a className="nav-link active" aria-current="page" href="#rsvp">RSVP</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        // <div id='invite' className={style.Navbar}>
+        //     <ul>
+        //         <a href='#home' className={style.menu}>HOME</a>
+        //         <a href='#invite' className={style.menu}>INVITE</a>
+        //         <a href='#timing' className={style.menu}>TIMING</a>
+        //         <a href='#venue' className={style.menu}>VENUE</a>
+        //         <img className={style.logo} src={Logo} alt="logo-j&y" />
+        //         <a href='#story' className={style.menu}>STORY</a>
+        //         <a href='#gallery' className={style.menu}>GALLERY</a>
+        //         <a href='#details' className={style.menu}>DETAILS</a>
+        //         <a href='#rsvp' className={style.menu}>RSVP</a>
+        //     </ul>
+        // </div>
     )
 }
 
-export default Navbar
+export default Navbar;
